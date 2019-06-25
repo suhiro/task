c369dfd8b94534de272d16c4a418f4d62fea7c4b
Hiro <suhiro@gmail.com>
2019-06-24 23:15:15 -0400

    bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('import:logs')->daily();
-        $schedule->command('test:scheduler')->everryMinute();
+        $schedule->command('test:scheduler')->everyMinute();
     }
 
     /**
