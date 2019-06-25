1574bf366c16793b9693bb9b0267185d2f2ed8e9
Hiro <suhiro@gmail.com>
2019-06-24 23:13:42 -0400

    schduler test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('import:logs')
-                 ->daily();
+        $schedule->command('import:logs')->daily();
+        $schedule->command('test:scheduler')->everryMinute();
     }
 
     /**
--- database/migrations/2019_06_25_030934_create_tests_table.php
+++ database/migrations/2019_06_25_030934_create_tests_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDeviceLogsTable extends Migration
+class CreateTestsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateDeviceLogsTable extends Migration
      */
     public function up()
     {
-        Schema::create('device_logs', function (Blueprint $table) {
+        Schema::create('tests', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedInteger('dsid');
-            $table->datetime('start');
-            $table->datetime('end')->nullable();
-            $table->boolean('on');
+            $table->string('status');
             $table->timestamps();
         });
     }
@@ -30,7 +27,6 @@ class CreateDeviceLogsTable extends Migration
      */
     public function down()
     {
-       
-        Schema::dropIfExists('device_logs');
+        Schema::dropIfExists('tests');
     }
 }
