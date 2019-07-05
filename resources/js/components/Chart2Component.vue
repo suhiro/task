33d051e36a74189fd272e962607a172fa2e24e06
Hiro <suhiro@gmail.com>
2019-07-05 01:50:45 -0400

    color coded

---
+++ resources/js/components/Chart2Component.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="chartdiv" class="chart">
+        chart2
+    </div>
+</template>
+
+<script>
+    import * as am4core from "@amcharts/amcharts4/core";
+    import * as am4charts from "@amcharts/amcharts4/charts";
+
+    export default {
+        name: "chart2-component",
+        mounted(){
+            let chart = am4core.create("chartdiv", am4charts.PieChart);
+
+            // Create pie series
+            let series = chart.series.push(new am4charts.PieSeries());
+            series.dataFields.value = "litres";
+            series.dataFields.category = "country";
+
+// Add data
+            chart.data = [{
+                "country": "Lithuania",
+                "litres": 501.9
+            }, {
+                "country": "Czech Republic",
+                "litres": 301.9
+            }, {
+                "country": "Ireland",
+                "litres": 201.1
+            }, {
+                "country": "Germany",
+                "litres": 165.8
+            }, {
+                "country": "Australia",
+                "litres": 139.9
+            }, {
+                "country": "Austria",
+                "litres": 128.3
+            }, {
+                "country": "UK",
+                "litres": 99
+            }, {
+                "country": "Belgium",
+                "litres": 60
+            }, {
+                "country": "The Netherlands",
+                "litres": 50
+            }];
+
+// And, for a good measure, let's add a legend
+            chart.legend = new am4charts.Legend();
+        }
+    }
+</script>
+
+<style scoped>
+div.chart {
+    height:500px;
+}
+</style>
--- resources/js/components/ChartComponent.vue
+++ resources/js/components/ChartComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello" ref="chartdiv">
+  <div class="chart" ref="chartdiv">
   </div>
 </template>
 
@@ -12,45 +12,61 @@ am4core.useTheme(am4themes_animated);
 
 
 export default {
-  name: 'HelloWorld',
+  name: 'chart-component',
   props: ['data'],
+    data() {
+      return {
+          logs:this.data
+      }
+
+    },
   mounted() {
-    console.log(this.data)
+      var colorSet = new am4core.ColorSet();
+      colorSet.saturation = 0.4;
+
+      _.forEach(this.logs, value => {
+         if(value.on == 1){
+             value.color = 'blue'
+         } else {
+             value.color = 'grey'
+         }
+      });
+    console.log(this.logs)
+
     let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
     chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
 
 chart.paddingRight = 30;
 chart.dateFormatter.inputDateFormat = "yyyy-MM-dd HH:mm:ss";
 
-var colorSet = new am4core.ColorSet();
-colorSet.saturation = 0.4;
 
-chart.data = [
-  {
-    dsid: "Device 1",
-    start: "2018-01-01 08:00",
-    end: "2018-01-01 08:05",
-  
-  },
-  {
-    dsid: "Device 1",
-    start: "2018-01-01 12:00",
-    end: "2018-01-01 15:00",
-  
-  },
-  {
-    dsid: "Device 1",
-    start: "2018-01-01 15:30",
-    end: "2018-01-01 21:30",
-  
-  },
 
-];
+// chart.data = [
+//   {
+//     dsid: "Device 1",
+//     start: "2018-01-01 08:00",
+//     end: "2018-01-01 08:05",
+//
+//   },
+//   {
+//     dsid: "Device 1",
+//     start: "2018-01-01 12:00",
+//     end: "2018-01-01 15:00",
+//
+//   },
+//   {
+//     dsid: "Device 1",
+//     start: "2018-01-01 15:30",
+//     end: "2018-01-01 21:30",
+//
+//   },
+//
+// ];
 
   chart.data = this.data;
 
   var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-  categoryAxis.dataFields.category = "dsid";
+  categoryAxis.dataFields.category = 'dsid';
   categoryAxis.renderer.grid.template.location = 0;
   categoryAxis.renderer.inversed = true;
 
@@ -58,8 +74,9 @@ chart.data = [
   dateAxis.dateFormatter.dateFormat = "yyyy-MM-dd HH:mm";
   dateAxis.renderer.minGridDistance = 60;
   dateAxis.baseInterval = { count: 1, timeUnit: "minute" };
-  dateAxis.max = new Date().getTime();
-  dateAxis.strictMinMax = true;
+  // dateAxis.max = new Date().getTime();
+      dateAxis.max = new Date('2019-07-02').getTime();
+  dateAxis.strictMinMax = false;
   dateAxis.renderer.tooltipLocation = 0;
 
   var series1 = chart.series.push(new am4charts.ColumnSeries());
@@ -69,6 +86,7 @@ chart.data = [
   series1.dataFields.openDateX = "start";
   series1.dataFields.dateX = "end";
   series1.dataFields.categoryY = "dsid";
+
   series1.columns.template.propertyFields.fill = "color"; // get color from data
   series1.columns.template.propertyFields.stroke = "color";
   series1.columns.template.strokeOpacity = 1;
@@ -87,7 +105,7 @@ chart.data = [
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.hello {
+.chart {
   width: 100%;
   height: 200px;
 }
