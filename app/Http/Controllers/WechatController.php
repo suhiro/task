f8a5b7ba1ab79753506821ad674e449a4703b821
Hiro <suhiro@gmail.com>
2019-08-29 20:25:00 -0400

    update

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Worker;
+use App\Workerlog;
 
 class WechatController extends Controller
 {
@@ -32,5 +34,28 @@ class WechatController extends Controller
         }
         return $response->getBody();
         $res = json_decode($response->getBody());
+        if(!$res->errcode){
+            $worker = Woker::where('wechat_openid',$res->openid)->first();
+            if($worker){
+//                        retrieve factory_name from db factories table with factory_id;
+//                    retrieve device list from devices table with factory_id;
+//
+//                    check if there are records of shift_logs;
+//                    if(exist){//微信用户已经有扫码上机的记录
+//                        return success with json data {"opeinid": , "factory_id": , "factory_name"：, "device_id": , ”device_name“, "checkin_time": };
+//                    }else{//微信用户没有扫码上任何机器
+//                        return success with json data {"opeinid": , "factory_id": , "factory_name"：};
+//                    }
+            } else {
+                return [
+                    'success' => false,
+                    'status' => 'error',
+                    'message' => '当前微信用户尚未注册到任何工厂，请先联系人事部门注册',
+                ];
+            }
+        } else {
+            return $res;
+        }
+
     }
 }
